cf4b540750a945a763f989f46bc16b6de00d22c3
Zarinabonu <zarinabonu199924@gmail.com>
2019-10-05 15:01:21 +0500

    cash

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+
+from app.model import Borrower
+
+
+class BorrowerList(ListView):
+    def get(self, request, *args, **kwargs):
+        b = Borrower.objects.all()
+        return render(request, 'administrator/borrower/create.html',{'borrower':b})
+
 
 # Create your views here.
--- urls.py
+++ urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.api.urls'))
+    path('api/', include('app.api.urls')),
+    path('', views.BorrowerList.as_view(), name='borrower-list')
 ]
